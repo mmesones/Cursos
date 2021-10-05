c6c6c0bf1f32c400f9327d4943f1ccb9fdba5f4b
Manuel <mmesones@intellisoftsa>
2021-10-05 18:06:39 +0000

    [ADD] Basic Module

---
+++ Odoo_Academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,32 @@
+# -.- coding: UTF-8 -*-
+
+{
+    'name':'Odoo_Academy',
+    
+    'summary': """Academy app to manage Training""",
+    
+    'description':"""
+     Academy Module to Manage Training:
+     -Courses
+     -Sessions
+     -Attendees
+    """,
+    
+    'Author': 'Odoo',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Training'.
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+    ],
+    
+    'demo': [
+        
+    ],
+    
+}
--- Odoo_Academy/__manifest__.py
+++ Odoo_Academy/__manifest__.py
@@ -17,7 +17,7 @@
     'website': 'https://www.odoo.com',
     
     'category': 'Training'.
-    'versionb': '0.1',
+    'version': '0.1',
     
     'depends': ['base'],
     
